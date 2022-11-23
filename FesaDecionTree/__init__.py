6385f0f99979a039ca741beeae4068c0ab47d32e
Felipe Soares <felipe.scsilva@outlook.com>
2022-11-23 12:01:49 -0300

    function azure

---
+++ FesaDecionTree/__init__.py
@@ -0,0 +1,53 @@
+import logging
+import tempfile
+import urllib.request
+from pickle import load
+
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    args = list()
+    logging.info('#1')
+    args.append(req.get_json().get('linguistica'))
+    args.append(req.get_json().get('logicoMatematico'))
+    args.append(req.get_json().get('espacial'))
+    args.append(req.get_json().get('corporal'))
+    args.append(req.get_json().get('musical'))
+    args.append(req.get_json().get('interpessoal'))
+    args.append(req.get_json().get('intrapessoal'))
+    args.append(req.get_json().get('naturalista'))
+
+    logging.info('#12.')
+
+    logging.info(args)
+    logging.info("\n\n")
+    logging.info(req.get_json)
+
+    if args:
+        return func.HttpResponse(multipleInteligence(args))
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+def multipleInteligence(args):
+    linguistica = float(args[0])
+    logicoMatematico = float(args[1])
+    espacial = float(args[2])
+    corporal = float(args[3])
+    musical = float(args[4])
+    interpessoal = float(args[5])
+    intrapessoal = float(args[6])
+    naturalista = float(args[7])
+
+    dir_path = tempfile.gettempdir()
+    # model = load(open(dir_path + '\dtree.pkl', 'rb'))
+    model = load(urllib.request.urlopen('https://apieleicoesrga6e1.blob.core.windows.net/pkl/dtree.pkl'))
+
+    predict = model.predict([[linguistica, logicoMatematico, espacial,
+                            corporal, musical, interpessoal, intrapessoal, naturalista]])
+
+
